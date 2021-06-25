327f12b8df5a0d85b3f6975b078c1762f2d97f5b
Joshua Robins <joshuakim220497@gmail.com>
2021-06-25 23:40:37 +0200

    Finished Database helper

--- BackendStuff/DB_Setup/db_setup.py
+++ BackendStuff/DB_Setup/db_setup.py
@@ -11,8 +11,8 @@ def setupDB():
 
     mycursor.execute("CREATE DATABASE IF NOT EXISTS TicketDB")
     mycursor.execute("USE TicketDB;")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS AvailableTickets (date VARCHAR(255) PRIMARY KEY UNIQUE, availableTicketsAdult INT)")
-    mycursor.execute("Create Table IF NOT EXISTS BookedTickets (bookingID INT Auto_Increment PRIMARY KEY UNIQUE, exponent INT, BigN INT, emailHash VARCHAR(255), date VARCHAR(15))")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS AvailableTickets (date VARCHAR(255) PRIMARY KEY UNIQUE, availableTickets INT)")
+    mycursor.execute("Create Table IF NOT EXISTS BookedTickets (bookingID INT Auto_Increment PRIMARY KEY UNIQUE, exponent INT, BigN INT, emailHash VARCHAR(255), nameHash VARCHAR(255), date VARCHAR(15))")
     mycursor.close()
 
 if __name__ == "__main__":
