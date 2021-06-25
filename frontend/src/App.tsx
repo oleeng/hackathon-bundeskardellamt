3468f47a588b8d16fa6065c2e017943a7cc12cea
Felix Wang <felix-wang@outlook.de>
2021-06-26 00:10:02 +0200

    implemented React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+} from "react-router-dom";
+
 import "./App.css";
 import { LandingPage } from "./Components/Pages/LandingPage/LandingPage";
 
 function App() {
-    return <LandingPage />;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <LandingPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
